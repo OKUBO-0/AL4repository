e6117f00642b6fdcad26c6adf6913cef50ebbd96
OKUBO-0 <k023g0020@g.neec.ac.jp>
2024-10-09 16:00:58 +0900

    AL3 03_01

---
+++ DirectXGame/scene/GameScene.cpp
@@ -0,0 +1,81 @@
+#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() {
+	delete model_;
+	delete player_;
+}
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+
+	// ファイルを指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("uvChecker.png");
+	// 3dモデルデータ
+	model_ = Model::Create();
+	// ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+	// 自キャラの生成
+	player_ = new Player();
+	// 自キャラの初期化
+	player_->Initialize(model_, textureHandle_);
+}
+
+void GameScene::Update() {
+	player_->Update();
+}
+
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+	
+	// 自キャラ描画
+	player_->Draw(viewProjection_);
+
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
